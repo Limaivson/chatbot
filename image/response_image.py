dea0575bc271856455ba49ef89cda6d5e4ad858b
Ivson <ils3@cin.ufpe.br>
2024-02-12 23:39:14 -0300

    feat(response-image)

--- image/response_image.py
+++ image/response_image.py
@@ -4,14 +4,13 @@ from api_key import read_key
 
 
 class ResponseImage:
-    def __init__(self, image_path, message=''):
+    def __init__(self):
         self.api_key = read_key.get_api_key()
         self.genai = genai.configure(api_key=f'{self.api_key}')
         self.model = genai.GenerativeModel('gemini-pro-vision')
-        self.image_path = image_path
-        self.image = PIL.Image.open(f'images/{self.image_path}.jpg')
-        self.message = message
 
-    def read_image(self):
-        response = self.model.generate_content([self.message, self.image])
-        return response.resolve()
+    def read_image(self, image_path, message):
+        image = PIL.Image.open(f'/Users/Ivson/Documents/GitHub/chatbot/image/imgs/car.jpeg')
+        response = self.model.generate_content([message, image])
+        response.resolve()
+        return response.text
